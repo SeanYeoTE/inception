4d25cd2c96a8b27d8890882f12dd142296c0ccda
Sean <tseensean@gmail.com>
2025-07-13 13:15:32 +0000

    wip

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE IF NOT EXISTS $(SQL_DATABASE);
-CREATE USER IF NOT EXISTS '$(MYSQL_USER)'@'%' IDENTIFIED BY '$(MYSQL_PASSWORD)';
-GRANT ALL PRIVILEGES ON $(SQL_DATABASE).* TO '$(MYSQL_USER)'@'%';
+-- This file is no longer used as database initialization is handled by setup.sh
+-- Environment variables are properly handled in the shell script
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';
 FLUSH PRIVILEGES;
